6f0c5e3351b2f45a069b8ff4125dd56f23125aef
fnl <florian.leitner@gmail.com>
2013-05-29 20:21:43 +0200

    refactored, final version 1

--- setup.py
+++ setup.py
@@ -7,7 +7,13 @@ setup(
     license='Apache License v2',
     author='Florian Leitner',
     author_email='florian.leitner@gmail.com',
-    url='http://www.github.com/fnl/patricia-trie',
+    url='http://www.github.com/fnl/patricia',
     py_modules=['patricia'],
+    classifiers=[
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
 )
-
