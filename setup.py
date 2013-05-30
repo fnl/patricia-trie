6759f6c81896afac06d5173d169e916d201fd1ea
fnl <florian.leitner@gmail.com>
2013-05-30 08:53:27 +0200

    fixed URL, added long_desc and classifiers

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from distutils.core import setup
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name='patricia-trie',
     version='1',
     description='A pure Python implementation of a PATRICIA trie.',
+    long_description=long_description,
     license='Apache License v2',
     author='Florian Leitner',
     author_email='florian.leitner@gmail.com',
-    url='http://www.github.com/fnl/patricia',
+    url='http://www.github.com/fnl/patricia-trie',
     py_modules=['patricia'],
     classifiers=[
-        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Indexing',
         'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
     ]
 )
